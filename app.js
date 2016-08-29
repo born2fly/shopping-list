440b35de0d48936cb0961b5008ad4ee656d83e64
Rich Leonard <rich@homeimprovement-li.com>
2016-08-29 11:08:53 -0400

    Now it works !

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 $(document).ready(function() {
+  console.log(' doc ready');
 
-//  -----------  prevent sending request -----------
- $('#js-shopping-list-form').submit(function(event) {
-   event.preventDefault();
-   console.log('event prevent triggered');
+  //  -----------  prevent sending request -----------
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    // console.log('event prevent triggered');
+    // console.log(event.target);
+    // console.log(  $(event.target)  );
+    // console.log(  $(event.target).find('#shopping-list-entry')  );
+
+    var myValue = $(event.target).find('#shopping-list-entry').val();
+    //  console.log(myValue);
+    //  console.log( $('.shopping-list') );
+
+    //  ---------- add item to bottom of list  ----------
+    $('.shopping-list').append('<li>' +
+      '<span class="shopping-item">' + myValue + '</span>' +
+      '<div class="shopping-item-controls">' +
+      '<button class="shopping-item-toggle">' +
+      '<span class="button-label">check</span>' +
+      '</button> ' +
+      '<button class="shopping-item-delete">' +
+      '<span class="button-label">delete</span>' +
+      '</button>' +
+      '</div>' +
+      '</li>');
+
+    //   ----------- reset input ------------
+    $(event.target).find('#shopping-list-entry').val('');
+
+  });
 
 
-     // ------------  remove item  -----------
-     $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-       $(this).closest('li').remove();
-        console.log('toggle remove item');
+  // ------------  remove item  -----------
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+    console.log('toggle remove item');
 
-       });
-
-
-      // ----------  toggle 'checked' items  ----------
-       $("shopping-list").on('click', 'shopping-item-toggle', (function(event) {
-        $(this).closest('li').toggleClass("shopping-item-checked");
-        console.log('toggle checked item');
-
-       });
-  
+  });
 
 
+  // ----------  toggle 'checked' items  ------------
+  $(".shopping-list").on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    console.log('toggle checked item');
 
- });
+  });
+
+
+
+});
---
+++ sample.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+      console.log(' doc ready');
+
+var shoppingList = $('.shopping-list').on('click', '.shopping-item-delete',function(event){
+           console.log('click');
+
+}); 
+        console.log(shoppingList);
+
+       
+   
+  
+});
+
+console.log(' page loading');
+
